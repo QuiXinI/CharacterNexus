2214d43b9dd0331b29c7c74772f6af43de5bf301
QuiXinI <quixini.q@gmail.com>
2026-05-31 00:32:16 +0300

    Version 1.6.3.1

--- app/src/main/java/ru/quasaris/characters/master/MainActivity.kt
+++ app/src/main/java/ru/quasaris/characters/master/MainActivity.kt
@@ -5,8 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.*
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -17,13 +16,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -50,124 +44,85 @@ class MainActivity : ComponentActivity() {
 
                 val navController = rememberNavController()
 
-                val springSpec = spring<IntOffset>(
-                    dampingRatio = Spring.DampingRatioNoBouncy,
-                    stiffness = Spring.StiffnessMedium
-                )
+                val animDuration = 550
+                val navHostOffsetSpec = tween<IntOffset>(durationMillis = animDuration, easing = FastOutSlowInEasing)
 
-                NavHost(navController = navController, startDestination = "menu") {
-                    composable(
-                        "menu",
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.surface,
+                ) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = "menu",
+                        enterTransition = {
+                            slideInHorizontally(initialOffsetX = { it }, animationSpec = navHostOffsetSpec)
+                        },
                         exitTransition = {
-                            slideOutHorizontally(targetOffsetX = { -it }, animationSpec = springSpec)
+                            slideOutHorizontally(targetOffsetX = { -it }, animationSpec = navHostOffsetSpec)
                         },
                         popEnterTransition = {
-                            if (initialState.destination.route == "create_setup") {
-                                slideInVertically(initialOffsetY = { -it }, animationSpec = springSpec)
-                            } else {
-                                slideInHorizontally(initialOffsetX = { -it }, animationSpec = springSpec)
-                            }
+                            slideInHorizontally(initialOffsetX = { -it }, animationSpec = navHostOffsetSpec)
+                        },
+                        popExitTransition = {
+                            slideOutHorizontally(targetOffsetX = { it }, animationSpec = navHostOffsetSpec)
                         }
                     ) {
-                        MenuWindow(
-                            characters = characters,
-                            onNavigateToCreate = { navController.navigate("create_setup") },
-                            onCharacterClick = { characterId ->
-                                navController.navigate("edit/$characterId")
-                            },
-                            onImportCharacter = { importedCharacter ->
-                                characters.add(importedCharacter)
-                                characterRepository.saveCharacters(characters)
-                            },
-                            onDeleteCharacters = { idsToDelete ->
-                                characters.removeAll { it.id in idsToDelete }
-                                characterRepository.saveCharacters(characters)
-                            }
-                        )
-                    }
-
-                    composable(
-                        "create_setup",
-                        enterTransition = {
-                            slideInVertically(initialOffsetY = { it }, animationSpec = springSpec)
-                        },
-                        exitTransition = {
-                            slideOutVertically(targetOffsetY = { it }, animationSpec = springSpec)
-                        },
-                        popExitTransition = {
-                            slideOutVertically(targetOffsetY = { it }, animationSpec = springSpec)
-                        }
-                    ) {
-                        CharacterCreationWindow(
-                            onNavigateBack = { navController.popBackStack() },
-                            onCharacterCreate = { newChar ->
-                                characters.add(newChar)
-                                characterRepository.saveCharacters(characters)
-                                navController.navigate("character_sheet/${newChar.id}") {
-                                    popUpTo("menu")
-                                }
-                            }
-                        )
-                    }
-
-                    composable(
-                        route = "character_sheet/{characterId}",
-                        arguments = listOf(navArgument("characterId") { type = NavType.IntType }),
-                        enterTransition = {
-                            slideInVertically(initialOffsetY = { it }, animationSpec = springSpec)
-                        },
-                        exitTransition = {
-                            slideOutHorizontally(targetOffsetX = { it }, animationSpec = springSpec)
-                        },
-                        popExitTransition = {
-                            slideOutHorizontally(targetOffsetX = { it }, animationSpec = springSpec)
-                        }
-                    ) { backStackEntry ->
-                        val characterId = backStackEntry.arguments?.getInt("characterId")
-                        val character = characters.find { it.id == characterId }
-                        
-                        CreateWindow(
-                            character = character,
-                            onNavigateBack = { navController.popBackStack("menu", false) },
-                            onCharacterChange = { updated ->
-                                val index = characters.indexOfFirst { it.id == updated.id }
-                                if (index != -1) {
-                                    characters[index] = updated
+                        composable(
+                            "menu"
+                        ) {
+                            MenuWindow(
+                                characters = characters,
+                                onNavigateToCreate = { navController.navigate("create_setup") },
+                                onCharacterClick = { characterId ->
+                                    navController.navigate("edit/$characterId")
+                                },
+                                onImportCharacter = { importedCharacter ->
+                                    characters.add(importedCharacter)
+                                    characterRepository.saveCharacters(characters)
+                                },
+                                onDeleteCharacters = { idsToDelete ->
+                                    characters.removeAll { it.id in idsToDelete }
                                     characterRepository.saveCharacters(characters)
                                 }
-                            }
-                        )
-                    }
-
-                    composable(
-                        route = "edit/{characterId}",
-                        arguments = listOf(navArgument("characterId") { type = NavType.IntType }),
-                        enterTransition = {
-                            slideInHorizontally(initialOffsetX = { it }, animationSpec = springSpec)
-                        },
-                        exitTransition = {
-                            slideOutHorizontally(targetOffsetX = { it }, animationSpec = springSpec)
-                        },
-                        popExitTransition = {
-                            slideOutHorizontally(targetOffsetX = { it }, animationSpec = springSpec)
+                            )
                         }
-                    ) { backStackEntry ->
-                        val characterId = backStackEntry.arguments?.getInt("characterId")
-                        val character = characters.find { it.id == characterId }
 
-                        CreateWindow(
-                            character = character,
-                            onNavigateBack = {
-                                navController.popBackStack()
-                            },
-                            onCharacterChange = { updatedCharacter ->
-                                val index = characters.indexOfFirst { it.id == updatedCharacter.id }
-                                if (index != -1) {
-                                    characters[index] = updatedCharacter
+                        composable(
+                            "create_setup"
+                        ) {
+                            CharacterCreationWindow(
+                                onNavigateBack = { navController.popBackStack() },
+                                onCharacterCreate = { newChar ->
+                                    characters.add(newChar)
                                     characterRepository.saveCharacters(characters)
+                                    navController.navigate("edit/${newChar.id}") {
+                                        popUpTo("menu")
+                                    }
                                 }
-                            }
-                        )
+                            )
+                        }
+
+                        composable(
+                            route = "edit/{characterId}",
+                            arguments = listOf(navArgument("characterId") { type = NavType.IntType })
+                        ) { backStackEntry ->
+                            val characterId = backStackEntry.arguments?.getInt("characterId")
+                            val character = characters.find { it.id == characterId }
+
+                            CreateWindow(
+                                character = character,
+                                onNavigateBack = {
+                                    navController.popBackStack()
+                                },
+                                onCharacterChange = { updatedCharacter ->
+                                    val index = characters.indexOfFirst { it.id == updatedCharacter.id }
+                                    if (index != -1) {
+                                        characters[index] = updatedCharacter
+                                        characterRepository.saveCharacters(characters)
+                                    }
+                                }
+                            )
+                        }
                     }
                 }
             }
